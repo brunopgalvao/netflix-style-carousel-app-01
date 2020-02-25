a3d9e5b12b91af12bd05109682c82753e83a5a8a
Bruno Galvao <brunopgalvao@gmail.com>
2020-02-25 16:33:06 -0500

    working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const wrapper = document.querySelectorAll('section')
+const cards = document.querySelectorAll('section')
 
 const forwardArrow = document.querySelector('.forward')
 const backArrow = document.querySelector('.back')
@@ -7,26 +7,30 @@ let counter = 1
 
 const enableForwardScroll = () => {
     let element = document.getElementById(`section${counter + 1}`)
-    if (counter === wrapper.length) {
-        counter = 0
-        document.querySelector(`#section${counter + 1}`).scrollIntoView()
+    if (counter === cards.length) {
+        counter = 1
+        document.querySelector(`#section${counter}`).scrollIntoView()
+        backArrow.classList = 'arrow back hide'
     } else {
         counter += 1
         element.scrollIntoView()
+        backArrow.classList = 'arrow back'
     }
 }
 
 const enableBackScroll = () => {
-    console.log(counter)
     let element = document.getElementById(`section${counter - 1}`)
-    if (counter === 1) {
-        counter = wrapper.length
-        document.querySelector(`#section${counter}`).scrollIntoView()
-    } else {
-        counter -= 1
-        element.scrollIntoView()
+    if (element != null) {
+        if (counter === 2) {
+            counter -= 1
+            element.scrollIntoView()
+            backArrow.classList = 'arrow back hide'
+        } else {
+            counter -= 1
+            element.scrollIntoView()
+        }
     }
 }
 
-forwardArrow.onclick = enableForwardScroll
-backArrow.onclick = enableBackScroll
+forwardArrow.addEventListener('click', enableForwardScroll)
+backArrow.addEventListener('click', enableBackScroll)
